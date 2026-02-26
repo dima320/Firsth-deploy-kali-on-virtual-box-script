45a80941c9a221d69d254b0017864ef82d7c08a4
dima320 <60215417+dima320@users.noreply.github.com>
2026-02-26 12:34:27 +0200

    main

---
+++ kali-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Updating sources.list..."
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware" | sudo tee /etc/apt/sources.list > /dev/null
+
+echo "Updating system..."
+sudo apt update -y
+sudo apt full-upgrade -y
+
+echo "Installing packages..."
+sudo apt install -y build-essential dkms linux-headers-$(uname -r) \
+virtualbox-guest-x11 virtualbox-guest-utils virtualbox-guest-dkms \
+kali-linux-default kali-tools-top10 git curl wget net-tools htop neovim
+
+echo "Done! Rebooting..."
+sudo reboot
